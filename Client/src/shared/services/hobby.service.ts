5e3edd112755d564056cb7e308035f9ce3a85cf4
Daniel Labella De la Cruz <daniel.labella@aktios.com>
2018-12-04 11:38:09 +0100

    added hobby service

--- Client/src/shared/services/hobby.service.ts
+++ Client/src/shared/services/hobby.service.ts
@@ -1,19 +1,20 @@
 import { Hobby } from '../models/hobby';
 import { Injectable } from '@angular/core';
+import { extend } from 'webdriver-js-extender';
+import { ServiceBase } from './serviceBase';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HobbyService {
+export class HobbyService extends ServiceBase {
 
-  public  GetHobbies(): Array<Hobby> {
-    const hobbies = new Array<Hobby>();
+  constructor(httpClient: HttpClient) {
+    super('http://127.0.0.1:3000/', httpClient);
+  }
 
-    const dotnet = new Hobby('.net', '.NET Programing');
-    const visualStudio = new Hobby('vs', 'Visual Studio');
-
-    hobbies.push(dotnet);
-    hobbies.push(visualStudio);
-    return hobbies;
+  public  GetHobbies(): Observable<Hobby[]> {
+    return this.apiGetCall('hobby');
   }
 }
