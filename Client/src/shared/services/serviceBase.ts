0549bdab8d7413f1bdcb4e77b00ceae923777f77
Daniel Labella De la Cruz <daniel.labella@aktios.com>
2018-12-11 12:32:48 +0100

    merge

--- Client/src/shared/services/serviceBase.ts
+++ Client/src/shared/services/serviceBase.ts
@@ -12,25 +12,7 @@ export abstract class ServiceBase {
   }
 
   public apiGetCall<T>(url: string): Observable<T> {
-    return this.httpClient.get<T>(this.getApiUrl(url)).pipe(
-      catchError(this.handleError('getCustomers', []))
-    );
-  }
-
-  handleError<T> (serviceName = '', operation = 'operation', result = {} as T) {
-    return (error: HttpErrorResponse): Observable<T> => {
-      // Todo -> Send the error to remote logging infrastructure
-      console.log(error); // log to console instead
-      const message = (error.error instanceof ErrorEvent) ?
-        error.error.message :
-       '{error code: ${error.status}, body: "${error.message}"}';
-      // -> Return a safe result.
-      return of( result );
-    };
-  }
-
-  private log(message: string) {
-    console.error(message);
+    return this.httpClient.get<T>(this.getApiUrl(url));
   }
 
   private getApiUrl(part: string): string {
